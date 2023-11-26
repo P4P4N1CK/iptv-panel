a1fd2d33e87408d2d2f4f830a1962456a204ce7f
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-11-26 12:20:14 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 clear
 
-read -p "Input Domain : " domain
+ipvps=$(curl -s https://ipv4.icanhazip.com)
 
-sudo apt update
-sudo apt upgrade -y
+# Fetch the panel IP from the provided URL
+panel_ips=$(curl -s https://raw.githubusercontent.com/syfqsamvpn/iptv/main/panel_access.txt)
 
-sudo apt install -y python3-pip
-sudo apt install git -y
-sudo certbot certonly --standalone -d ${domain}
-git clone https://github.com/syfqsamvpn/iptv-panel.git
-echo "$domain" >/root/iptv-panel/domain.txt
-cd /root/iptv-panel
-pip3 install -r requirements.txt
+# Check if the current server's IP matches the panel IP
+if [ "$(echo "${panel_ips}" | grep -wc "$ipvps")" != '0' ]; then
+    read -p "Input Domain: " domain
 
-mv /root/iptv-panel/menu.sh /usr/bin/menu.sh
-mv /root/iptv-panel/run.sh /usr/bin/run.sh
+    sudo apt update
+    sudo apt upgrade -y
 
-chmod +x /usr/bin/menu.sh
-chmod +x /usr/bin/run.sh
-(crontab -l ; echo "0 0 * * * reboot") | crontab -
-(crontab -l ; echo "10 0 * * * run.sh") | crontab -
-run.sh
-sudo -i
+    sudo apt install -y python3-pip
+    sudo apt install git -y
+    sudo certbot certonly --standalone -d ${domain}
+    git clone https://github.com/syfqsamvpn/iptv-panel.git
+    echo "$domain" >/root/iptv-panel/domain.txt
+    cd /root/iptv-panel
+    pip3 install -r requirements.txt
+
+    mv /root/iptv-panel/menu.sh /usr/bin/menu.sh
+    mv /root/iptv-panel/run.sh /usr/bin/run.sh
+
+    chmod +x /usr/bin/menu.sh
+    chmod +x /usr/bin/run.sh
+    (
+        crontab -l
+        echo "0 0 * * * reboot"
+    ) | crontab -
+    (
+        crontab -l
+        echo "10 0 * * * run.sh"
+    ) | crontab -
+    run.sh
+    sudo -i
+else
+    echo "Access denied. Panel IP matches server IP."
+    exit 1
+fi
