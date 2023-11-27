6782a63a664b86738ef6d446e6231e14642ea378
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-11-27 13:49:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
 domain=$(sed -n '1p' /root/iptv-panel/domain.txt)
+cpu=$(nproc)
 cd /root
 pkill -f "gunicorn.*main:app" >/dev/null 2>&1
-bash -c "cd '/root/iptv-panel' && gunicorn -w 1 -b 0.0.0.0:443 --keyfile /etc/letsencrypt/live/${domain}/privkey.pem --certfile /etc/letsencrypt/live/${domain}/fullchain.pem --preload main:app --daemon"
+bash -c "cd '/root/iptv-panel' && gunicorn -w ${cpu} -b 0.0.0.0:443 --keyfile /etc/letsencrypt/live/${domain}/privkey.pem --certfile /etc/letsencrypt/live/${domain}/fullchain.pem --preload main:app --daemon"
