8010aa87fcf65f161d0ff55c5bfea3b70c32792b
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-11-28 02:52:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ cpu=$(nproc)
 cd /root
 pkill -f "gunicorn.*main:app" >/dev/null 2>&1
 bash -c "cd '/root/iptv-panel' && gunicorn -w ${cpu} -b 0.0.0.0:443 --keyfile /etc/letsencrypt/live/${domain}/privkey.pem --certfile /etc/letsencrypt/live/${domain}/fullchain.pem --preload main:app --daemon"
+screen -dmS panel_bot ott_sam.sh
