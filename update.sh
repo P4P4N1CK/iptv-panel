a1370acbdce27640c313cc2af0bd09c266bdf034
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-11-29 22:11:26 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,14 +3,16 @@ clear
 
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh
 chmod +x /usr/bin/ott_sam.sh
-
-(
-    crontab -l
-    echo "0 12 * * * ott_sam.sh -a > /root/t1.log 2>&1"
-) | crontab -
-
-(
-    crontab -l
-    echo "0 13 * * * ott_sam.sh -b > /root/t1.log 2>&1"
-) | crontab -
-run.sh
+if ! crontab -l | grep -q "ott_sam.sh -a"; then
+    (
+        crontab -l
+        echo "0 12 * * * ott_sam.sh -a > /root/t1.log 2>&1"
+    ) | crontab -
+fi
+if ! crontab -l | grep -q "ott_sam.sh -b"; then
+    (
+        crontab -l
+        echo "0 13 * * * ott_sam.sh -b > /root/t1.log 2>&1"
+    ) | crontab -
+    run.sh
+fi
