ef067fb3d063bc309a9fd4cc2c7c268f7ed2b10a
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-11-29 22:34:20 +0800

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -24,6 +24,18 @@ firtsTimeRun() {
 ipvps=$(curl -s "https://ipv4.icanhazip.com")
 if [ "$(curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/panel_bot.sh" | grep -wc "${ipvps}")" != '0' ]; then
     firtsTimeRun
+    if ! crontab -l | grep -q "ott_sam.sh -a"; then
+        (
+            crontab -l
+            echo "0 12 * * * ott_sam.sh -a > /root/t1.log 2>&1"
+        ) | crontab -
+    fi
+    if ! crontab -l | grep -q "ott_sam.sh -b"; then
+        (
+            crontab -l
+            echo "0 13 * * * ott_sam.sh -b > /root/t1.log 2>&1"
+        ) | crontab -
+    fi
 else
     rm -rf "/root/ottbot"
     crontab -l | grep -v 'start_bot.sh' | crontab -
