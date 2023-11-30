f2e5e24c32655802b8785965314bfc15522d169a
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-11-30 20:43:09 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,12 +2,14 @@
 clear
 
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh
+curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" >/usr/bin/menu
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/template.txt" >/root/iptv-panel/template.txt
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
 
 chmod +x /usr/bin/ott_sam.sh
+chmod +x /usr/bin/menu
 ipvps=$(curl -s "https://ipv4.icanhazip.com")
 if [ "$(curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/panel_bot.sh" | grep -wc "${ipvps}")" != '0' ]; then
     if ! crontab -l | grep -q "ott_sam.sh -a"; then
