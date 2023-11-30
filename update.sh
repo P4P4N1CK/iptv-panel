2144345913890fb9d7421081706282f4ae2e2686
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-11-30 19:07:05 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,21 +3,24 @@ clear
 
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/template.txt" >/root/iptv-panel/template.txt
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/__init__.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/_pytransform.so
+curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/template.txt
+curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/template.txt
 
 chmod +x /usr/bin/ott_sam.sh
-if ! crontab -l | grep -q "ott_sam.sh -a"; then
-    (
-        crontab -l
-        echo "0 12 * * * ott_sam.sh -a > /root/t1.log 2>&1"
-    ) | crontab -
-fi
-if ! crontab -l | grep -q "ott_sam.sh -b"; then
-    (
-        crontab -l
-        echo "0 13 * * * ott_sam.sh -b > /root/t1.log 2>&1"
-    ) | crontab -
+ipvps=$(curl -s "https://ipv4.icanhazip.com")
+if [ "$(curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/panel_bot.sh" | grep -wc "${ipvps}")" != '0' ]; then
+    if ! crontab -l | grep -q "ott_sam.sh -a"; then
+        (
+            crontab -l
+            echo "0 12 * * * ott_sam.sh -a > /root/t1.log 2>&1"
+        ) | crontab -
+    fi
+    if ! crontab -l | grep -q "ott_sam.sh -b"; then
+        (
+            crontab -l
+            echo "0 13 * * * ott_sam.sh -b > /root/t1.log 2>&1"
+        ) | crontab -
+    fi
 fi
 
 run.sh
