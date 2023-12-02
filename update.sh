57871f4db96f566c6257d268f0776db7b125466b
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-12-02 22:46:57 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,30 +3,14 @@ clear
 
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" >/usr/bin/menu
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/data.txt" >/root/iptv-panel/data.txt
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/template.txt" >/root/iptv-panel/template.txt
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/secure_short.json" >/root/iptv-panel/secure_short.json
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/banned.m3u" >/root/iptv-panel/banned.m3u
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
+curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
 
 chmod +x /usr/bin/ott_sam.sh
 chmod +x /usr/bin/menu
-ipvps=$(curl -s "https://ipv4.icanhazip.com")
-if [ "$(curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/panel_bot.sh" | grep -wc "${ipvps}")" != '0' ]; then
-    if ! crontab -l | grep -q "ott_sam.sh -a"; then
-        (
-            crontab -l
-            echo "0 12 * * * ott_sam.sh -a > /root/t1.log 2>&1"
-        ) | crontab -
-    fi
-    if ! crontab -l | grep -q "ott_sam.sh -b"; then
-        (
-            crontab -l
-            echo "0 13 * * * ott_sam.sh -b > /root/t1.log 2>&1"
-        ) | crontab -
-    fi
-fi
+
+echo "EXPIRED_DATA = \"expired.json\"" >/root/iptv-panel/data.txt
 
 run.sh
