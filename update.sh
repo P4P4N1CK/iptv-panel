c5e981acfd251550f8aabcd9154990e18474791c
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-12-05 16:12:51 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,8 +7,9 @@ curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
+curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sniff_template.txt" >/root/iptv-panel/sniff_template.txt
+curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/multilogin_template.txt" >/root/iptv-panel/multilogin_template.txt
 
 chmod +x /usr/bin/ott_sam.sh
 chmod +x /usr/bin/menu
 
-run.sh
