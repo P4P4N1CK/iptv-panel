684b6e52a9fb0994e5e1b85c7b9841641e01898f
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-12-21 21:34:13 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 clear
 timedatectl set-timezone Asia/Kuala_Lumpur
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" >/usr/bin/menu
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
-
-chmod +x /usr/bin/ott_sam.sh
-chmod +x /usr/bin/menu
-
